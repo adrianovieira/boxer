2f24ccde9e87fedc2b5363ebcc1a4cbe3d5de1f0
Adriano Vieira <adriano.svieira@gmail.com>
2016-07-29 23:09:11 -0300

    Update docker-engine version to install (1.12.0-GA)
    - update docker-compose version (1.8.0)

--- centos7/docker-install.sh
+++ centos7/docker-install.sh
@@ -8,19 +8,20 @@ fi
 if [[ "$ID" == "centos" && "$VERSION_ID" == "7" ]]; then
   echo "INFO: [docker-install.sh] importing gpg key from dockerproject"
   sudo rpm --import https://yum.dockerproject.org/gpg
+  sudo cp /home/vagrant/sync/centos7/yum.repos.d/* /etc/yum.repos.d/
   sudo yum clean all
   sudo yum update -y -q
-  sudo cp /home/vagrant/sync/centos7/yum.repos.d/* /etc/yum.repos.d/
 
   echo "INFO: [docker-install.sh] install docker-engine"
-  sudo yum install -y docker-engine-1.12.0-0.3.rc3.el7.centos
+  sudo yum install -y docker-engine-1.12.0-1.el7.centos
 
   echo "INFO: [docker-install.sh] start docker-engine"
   sudo systemctl daemon-reload
   sudo systemctl enable docker && sudo systemctl start docker
 
   echo "INFO: [docker-install.sh] install docker-compose"
-  sudo curl -L https://github.com/docker/compose/releases/download/1.8.0-rc2/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
+  #sudo curl -L https://github.com/docker/compose/releases/download/1.8.0-rc2/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
   sudo chmod +x /tmp/docker-compose
   sudo mv /tmp/docker-compose /usr/local/bin/docker-compose
   sudo rm -f /tmp/docker-compose
