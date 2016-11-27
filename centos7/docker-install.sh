0d3fc1234d2c9838c2f5db3e4cf8d5dc17eeaeea
Adriano Vieira <adriano.svieira@gmail.com>
2016-11-27 00:45:03 -0200

    Update version of docker and docker-compose

--- centos7/docker-install.sh
+++ centos7/docker-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Install docker-engine-1.12.0-rc3 and docker-compose-1.8.0-rc2
+# Install docker-engine-1.12 and docker-compose-1.9
 
 if [[ -f /etc/os-release  ]]; then
   . /etc/os-release
@@ -13,14 +13,15 @@ if [[ "$ID" == "centos" && "$VERSION_ID" == "7" ]]; then
   sudo yum update -y -q
 
   echo "INFO: [docker-install.sh] install docker-engine"
-  sudo yum install -y docker-engine-1.12.1-1.el7.centos
+  #sudo yum install -y docker-engine-1.12.1-1.el7.centos
+  sudo yum install -y docker-engine
 
   echo "INFO: [docker-install.sh] start docker-engine"
   sudo systemctl daemon-reload
   sudo systemctl enable docker && sudo systemctl start docker
 
   echo "INFO: [docker-install.sh] install docker-compose"
-  sudo curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
   sudo chmod +x /tmp/docker-compose
   sudo mv /tmp/docker-compose /usr/local/bin/docker-compose
   sudo rm -f /tmp/docker-compose
