614cc9a4a3d3d7654d2a8a75a890616a0093f80b
Adriano Vieira <adriano.svieira@gmail.com>
2017-01-02 12:20:14 -0200

    Update debian8 cleanup

--- debian8/housekeeper-cleanup.sh
+++ debian8/housekeeper-cleanup.sh
@@ -6,6 +6,9 @@ fi
 
 if [[ "$ID" == "debian" && "$VERSION_ID" == "8" ]]; then
   sudo rm -f /etc/profile.d/proxy.sh
+  sudo apt-get clean
+  sudo apt-get autoclean
 fi
 
 sudo rm -rf /home/vagrant/sync
+sudo rm -rf /vagrant
